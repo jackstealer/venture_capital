4b6343cb759f6742b691c24d6791aa565d988f68
Arman Singh <armaning313@gmail.com>
2026-04-24 01:36:43 +0530

    Removed test_mongo due to leaked credentials

--- backend/test_mongo.py
+++
@@ -1,9 +0,0 @@
-# save as test_mongo.py and run from backend/
-from pymongo import MongoClient
-import os
-uri = os.getenv('MONGO_URI', 'mongodb+srv://armanopdc:arman123@cluster0.yv6hvjv.mongodb.net/?appName=Cluster0')
-client = MongoClient(uri, serverSelectionTimeoutMS=5000)
-try:
-    print(client.admin.command('ping'))
-except Exception as e:
-    print('Connect error:', repr(e))
